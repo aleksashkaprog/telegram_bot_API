2ef7d39dfac39005b46519af2a7ff25d41d9814f
aleksashkaprog <lalalalala.7la@gmail.com>
2022-06-04 10:36:21 +0800

    lowprice_begin

--- botrequests/lowprice.py
+++ botrequests/lowprice.py
@@ -1,9 +1,43 @@
-import main
-from main import bot
+from pprint import pprint
 
-@bot.message_handler(content_types=['text'])
-def commands_bot(message):
-    if message.text == '/lowprice':
-        city = input('Введите город, куда хотите поехать: ')
-        hotels_count = int(input('Какое количество отелей вы хотите посмотреть? (от 1 до 10)'))
-        main.bot.send_message(message.from_user.id, main.querystring)
+import requests
+from decouple import config
+
+API_KEY = config('rapidapi-key')
+
+
+def get_lowprice(city, hotels_count):
+    url = "https://hotels4.p.rapidapi.com/locations/v2/search"
+
+    querystring = {"query": f'{city}'}
+
+    headers = {
+        "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
+        "X-RapidAPI-Key": API_KEY
+    }
+
+    lowprice_list_id = []
+    lowprice_list_name = []
+    hotels_list = []
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    found_city = response.json()
+    for i in range(len(found_city["suggestions"])):
+        for j in range((len(found_city["suggestions"][i]["entities"]))):
+            if found_city["suggestions"][i]["entities"][j]["type"] == "HOTEL":
+                lowprice_list_id.append(found_city["suggestions"][i]["entities"][j]['destinationId'])
+                lowprice_list_name.append(found_city["suggestions"][i]["entities"][j]['name'])
+
+    for i in lowprice_list_id:
+        url_hotel = "https://hotels4.p.rapidapi.com/properties/list"
+
+        querystring_hotel = {"destinationId": f'{i}', "sortOrder": "PRICE"}
+
+        response_hotel = requests.request("GET"[:hotels_count], url_hotel, headers=headers, params=querystring_hotel)
+
+        found_hotel = response_hotel.json()
+
+        hotels_list.append(found_hotel)
+    return hotels_list
+
+
+pprint(get_lowprice('New York', 1))
--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 #coding: utf-8
 import os
 import telebot
-import requests
 from decouple import config
-
-url = "https://hotels4.p.rapidapi.com/locations/search"
-querystring = {"query": "new york", "locale": "en_US"}
-headers = {
-    'x-rapidapi-host': "hotels4.p.rapidapi.com",
-    'x-rapidapi-key': "00bce2701emsh37560fcb97ec654p1ec40ejsn4f564a3f117b"
-    }
-response = requests.request("GET", url, headers=headers, params=querystring)
 
 os.environ['token_tg'] = 'token'
 TOKEN = os.getenv('token_tg')
@@ -19,18 +10,70 @@ bot = telebot.TeleBot(config(TOKEN))
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == 'Привет'.lower() or message.text == '/helloworld':
+    if message.text.lower() == 'привет' or message.text == '/helloworld':
         bot.send_message(message.from_user.id, 'Здравствуйте! '
                                                'Вас приветствует телеграм-бот турагенства "Too Easy Travel". '
-                                               'Чем я могу помочь Вам?')
+                                               'Чем я могу помочь Вам?'
+                                               'Доступные команды:'
+                                               '/lowprice - подборка самых дешевых отелей;'
+                                               '/highprice - подборка самых дорогих отелей;'
+                                               '/bestdeal - вывод отелей, наиболее подходящих по цене '
+                                               'и расположению от центра')
+
     elif message.text == '/help':
-        bot.send_message(message.from_user.id, 'Напишите Привет:)')
+        bot.send_message(message.from_user.id, 'Доступные команды:'
+                                               '/lowprice - подборка самых дешевых отелей;'
+                                               '/highprice - подборка самых дорогих отелей;'
+                                               '/bestdeal - вывод отелей, наиболее подходящих по цене '
+                                               'и расположению от центра')
+
+    elif message.text == '/lowprice' or message.text == '/highprice':
+        bot.send_message(message.from_user.id, 'Введите город, в который хотите поехать')
+        bot.register_next_step_handler(message, check_in_date)
+
+    elif message.text == '/bestdeal':
+        bot.send_message(message.from_user.id, 'Введите город, в который хотите поехать')
+        bot.register_next_step_handler(message, low)
 
     else:
-        bot.send_message(message.from_user.id, 'Я не понимаю, используйте команду /help')
+        bot.send_message(message.from_user.id, 'Я не понимаю эту команду, '
+                                               'напишите "/help" для просмотра доступных команд')
 
 
+def check_in_date(message):
+    bot.send_message(message.from_user.id, 'Введите дату предполагаемого заезда')
+    bot.register_next_step_handler(message, check_out_date)
 
+
+def check_out_date(message):
+    bot.send_message(message.from_user.id, 'Введите дату предполагаемого выселения')
+    bot.register_next_step_handler(message, hotels_count)
+
+def hotels_count(message):
+    bot.send_message(message.from_user.id, 'Сколько отелей Вы хотите посмотреть (не более 10)?')
+    bot.register_next_step_handler(message, is_photos)
+
+
+def is_photos(message):
+    bot.send_message(message.from_user.id, 'Хотите ли Вы посмотреть фотографии отелей?')
+    if message.text.lower() == 'да':
+        bot.register_next_step_handler(message, get_photos)
+    # else:
+    #     bot.send_message(message.from_user.id, get_lowprice())
+
+
+def get_photos(message):
+    bot.send_message(message.from_user.id, 'Сколько фотографий Вы хотите посмотреть (не более 10)?')
+    # bot.send_message(message.from_user.id, get_lowprice())
+
+
+def low(message):
+    bot.send_message(message.from_user.id, 'Укажите самую дешевую цену, устраивающую вас')
+    bot.register_next_step_handler(message, high)
+
+def high(message):
+    bot.send_message(message.from_user.id, 'Укажите самую дорогую цену, устраивающую вас')
+    bot.register_next_step_handler(message, hotels_count)
 
 
 bot.polling()
